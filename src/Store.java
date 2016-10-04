95d97f376c5a924f9702ea672be285d078ddc915
Ludovic56 <l.le.cornec@iia-laval.fr>
2016-10-04 23:03:38 +0200

    Coded methods

--- src/Store.java
+++ src/Store.java
@@ -1,49 +1,117 @@
+import java.awt.List;
 import java.util.ArrayList;
+
+import com.iia.shop.entity.Vehicle;
 
 public class Store {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		java.io.Console console = System.console();
-		
-		ArrayList vehicleArrayList = new ArrayList();
+		Vehicle vehicle = new Vehicle();
+
 		int mainMenuChoice = 0;
-		
-		//Main menu
-		do{
+		int chosenVehicle = 0;
+		int chosenVehicleMenuChoice = 0;
+
+		// Vehicle attributes
+		String brand;
+		int year;
+		String model;
+		String color;
+		double price;
+		int speed = 0;
+
+		// Main menu
+		do {
 			System.out.println("1 - Créer un véhicule");
 			System.out.println("2 - Voir tous les véhicules");
 			System.out.println("3 - Voir un véhicule ne particulier");
-			
+
 			System.out.println("0 - Quitter");
-			
+
 			mainMenuChoice = Integer.parseInt(console.readLine());
-			
-			switch(mainMenuChoice){
+
+			switch (mainMenuChoice) {
 			case 1:
-				
+				// Create vehicle
+				vehicle.createVehicle();
+
 				break;
-				
+
 			case 2:
-				
+				// Show all vehicles
+				vehicle.showAllVehicle();
+
 				break;
-				
+
 			case 3:
+				// Show the chosen vehicle
+				System.out.println("Choisir un véhicule");
+				chosenVehicle = Integer.parseInt(console.readLine());
+				vehicle.showOneVehicle(chosenVehicle);
+
+				// Chose to delete or modify selected vehicle
+				System.out.println("1 - Supprimer un véhicule");
+				System.out.println("2 - Modifier un véhicule");
+				System.out.println("3 - Augmenter la vitesse");
+				System.out.println("4 - Réduire la vitesse");
 				
+				
+				System.out.println("0 - Quitter");
+				chosenVehicleMenuChoice = Integer.parseInt(console.readLine());
+
+				switch (chosenVehicleMenuChoice) {
+				case 1:
+					// Delete chosen vehicle
+					vehicle.deleteVehicle(chosenVehicle);
+
+					break;
+
+				case 2:
+					//Modify a vehicle
+					vehicle.modifyVehicle(chosenVehicle);
+					break;
+					
+				case 3:
+					//Augment vehicle speed
+					System.out.println("Saisir la vitesse");
+					speed = Integer.parseInt(console.readLine());
+					
+					vehicle.speedUp(speed, chosenVehicle);
+					break;
+					
+				case 4:
+					//Reduce vehicle speed
+					System.out.println("Saisir la vitesse");
+					speed = Integer.parseInt(console.readLine());
+					
+					vehicle.speedDown(speed, chosenVehicle);
+					break;
+
+				case 0:
+					//Exit show vehicle method
+					break;
+
+				default:
+					System.out.println("Le véhicule n'existe pas");
+					break;
+				}
+
 				break;
-			
+
 			case 0:
+				// Exit program
 				System.out.println("A bientot !!");
 				break;
-				
+
 			default:
 				System.out.println("Erreur de choix main menu");
 				break;
 			}
-			
-		}while(mainMenuChoice != 0);		
+
+		} while (mainMenuChoice != 0);
 	}
-	
-	
+
 }
--- src/com/iia/shop/entity/Vehicle.java
+++ src/com/iia/shop/entity/Vehicle.java
@@ -1,6 +1,10 @@
 package com.iia.shop.entity;
 
+import java.util.ArrayList;
+
 public class Vehicle {
+	java.io.Console console = System.console();
+	
 	//Attributes
 	private String brand;
 	private int year;
@@ -9,6 +13,8 @@ public class Vehicle {
 	private double price;
 	private int speed;
 	
+	ArrayList<Vehicle> vehicleArrayList = new ArrayList();
+	int listSize;
 	
 	//Getters & Setters
 	public String getBrand() {
@@ -46,8 +52,7 @@ public class Vehicle {
 	}
 	public void setSpeed(int speed) {
 		this.speed = speed;
-	}
-	
+	}	
 	
 	//Constructors
 	public Vehicle(String brand, int year, String model, String color, double price) {
@@ -57,36 +62,83 @@ public class Vehicle {
 		this.model = model;
 		this.color = color;
 		this.price = price;
+		this.speed = 0;
+	}
+	
+	public Vehicle() {
+		super();
+		this.speed = 0;
 	}
 	
 	//Methods
 	
+	
 	public void createVehicle(){		
+		System.out.println("Saisir une marque");
+		brand = console.readLine();
+		System.out.println("Saisir une année");
+		year = Integer.parseInt(console.readLine());
+		System.out.println("Saisir une modèle");
+		model = console.readLine();
+		System.out.println("Saisir une couleur");
+		color = console.readLine();
+		System.out.println("Saisir une prix");
+		price = Double.parseDouble(console.readLine());
 		
+		Vehicle vehicle = new Vehicle(brand, year, model, color, price);
+		vehicleArrayList.add(vehicle);
 	}
 	
-	public void modifyVehicle(){
+	public void modifyVehicle(int choice){
+		System.out.println("Saisir une marque");
+		brand = console.readLine();
+		System.out.println("Saisir une année");
+		year = Integer.parseInt(console.readLine());
+		System.out.println("Saisir une modèle");
+		model = console.readLine();
+		System.out.println("Saisir une couleur");
+		color = console.readLine();
+		System.out.println("Saisir une prix");
+		price = Double.parseDouble(console.readLine());
 		
+		vehicleArrayList.get(choice).setBrand(brand);
+		vehicleArrayList.get(choice).setYear(year);
+		vehicleArrayList.get(choice).setModel(model);
+		vehicleArrayList.get(choice).setColor(color);
+		vehicleArrayList.get(choice).setPrice(price);
 	}
 	
-	public void deleteVehicle(){
-		
+	public void deleteVehicle(int choice){
+		vehicleArrayList.remove(choice);
 	}
 	
 	public void showAllVehicle(){
-		
+		for (listSize = 0; listSize < vehicleArrayList.size(); listSize++) {
+			System.out.println("Marque :" + vehicleArrayList.get(listSize).getBrand());
+			System.out.println("Année :" + vehicleArrayList.get(listSize).getYear());
+			System.out.println("Modèle :" + vehicleArrayList.get(listSize).getModel());
+			System.out.println("Couleur :" + vehicleArrayList.get(listSize).getColor());
+			System.out.println("Prix :" + vehicleArrayList.get(listSize).getPrice());
+		}
 	}
 	
-	public void showOneVehicle(){
-		
+	public void showOneVehicle(int choice){
+		System.out.println("Marque :" + vehicleArrayList.get(choice).getBrand());
+		System.out.println("Année :" + vehicleArrayList.get(choice).getYear());
+		System.out.println("Modèle :" + vehicleArrayList.get(choice).getModel());
+		System.out.println("Couleur :" + vehicleArrayList.get(choice).getColor());
+		System.out.println("Prix :" + vehicleArrayList.get(choice).getPrice());
+		System.out.println("Speed :" + vehicleArrayList.get(choice).getSpeed());
 	}
 	
-	public void speedUp(){
-		
+	public int speedUp(int speed, int choice){
+		vehicleArrayList.get(choice).setSpeed(speed);
+		return speed;
 	}
 	
-	public void speedDown(){
-		
+	public int speedDown(int speed, int choice){
+		vehicleArrayList.get(choice).setSpeed(speed);
+		return speed;
 	}
 	
 }
