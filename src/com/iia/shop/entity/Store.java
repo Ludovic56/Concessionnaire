5fb1e93de34e5b76be98e46f24ebc717d32029cf
Ludovic56 <l.le.cornec@iia-laval.fr>
2016-10-04 09:54:03 +0200

    Creating project
    Creating Store & vehicle class

---
+++ src/com/iia/shop/entity/Store.java
@@ -0,0 +1,10 @@
+package com.iia.shop.entity;
+
+public class Store {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/iia/shop/entity/Vehicle.java
@@ -0,0 +1,5 @@
+package com.iia.shop.entity;
+
+public class Vehicle {
+
+}
